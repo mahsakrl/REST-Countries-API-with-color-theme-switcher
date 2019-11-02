03a2aff642c301ceda1466d9b83393714a3a1ed7
mahsa karamloo <mahsa.k837@gmail.com>
2019-11-02 21:27:46 +0330

    countries card inf

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    darkMode: false,
+    routingParams: {}
   },
+  plugins: [createPersistedState()],
   mutations: {
+    darkMode (state, payload) {
+      state.darkMode = payload.payload
+    },
+    params (state, payload) {
+      state.routingParams = { ...payload.payload }
+      console.log('mut', state.routingParams)
+    }
   },
   actions: {
+    darkMode ({ commit, state }, payload) {
+      console.log(state)
+      commit('darkMode', payload)
+    },
+    params ({ commit }, payload) {
+      console.log('dark', payload)
+      commit('params', payload)
+    }
   },
   modules: {
   }
